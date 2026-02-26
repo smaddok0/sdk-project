e698dcd1a33daeb5210b669d1d51ad75bf892825
smaddok0 <tokekgoreng000@gmail.com>
2026-02-26 19:27:02 +0700

    vote request vote1

--- web streaming/app.py
+++ web streaming/app.py
@@ -1,7 +1,29 @@
-from flask import Flask,render_template,request,session
+from flask import Flask,render_template,request,session,jsonify
 import mysql.connector
 
 app = Flask(__name__)
+
+db = mysql.connector.connect(
+    user = 'freedb_smaddok',
+    password = 'T#Ff9CR4fuHFGBM',
+    host = 'sql.freedb.tech',
+    database = 'freedb_sdk_db',
+    port ='3306'
+)
+cs = db.cursor()
+
+# cs.execute("ALTER TABLE vote ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+# db.commit()
+
+# cs.execute("DELETE FROM vote WHERE id=%s",(6,))
+# db.commit()
+
+# cs.execute("UPDATE vote SET vote1 = %s", (0,))
+# db.commit()
+# cs.execute("SELECT * FROM vote")
+# for x in cs:
+#     print(x)
+
 
 # @app.route('/data_daftar',methods=['POST'])
 # def data_pendaftar():
@@ -10,7 +32,14 @@ app = Flask(__name__)
 #     return render_template('daftar.html',nama=nama,password=password)
 
 
-
+@app.route('/content/vote/add1')
+def addVote():
+    cs.execute("SELECT vote1 FROM vote")
+    votedb = cs.fetchone()[0]
+    votedb += 1
+    cs.execute("UPDATE vote SET vote1=%s WHERE id=%s",(votedb,1))
+    db.commit()
+    return jsonify(votedb)
 #------------------halaman---------------------
 @app.route("/")
 def utama():
@@ -32,3 +61,6 @@ def vote():
 def streaming():
     return render_template('content/streaming.html')
 
+# app.run(debug=True)
+cs.close()
+db.close()
