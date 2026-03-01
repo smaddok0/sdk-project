1502dfd95e9c6f74a3bc1dbc1c157db202c29a46
smaddok0 <tokekgoreng000@gmail.com>
2026-03-01 22:34:46 +0700

    adding vote 2 and3 system

--- web streaming/static/vote/vote.js
+++ web streaming/static/vote/vote.js
@@ -2,36 +2,67 @@
 let h1 = 0
 let h2 = 0
 let h3 = 0
+// ----------vote1-------------
+document.addEventListener("DOMContentLoaded",function preview1() {
+    fetch('/content/vote/preview1')
+        .then(res => res.json())
+        .then(vote1)
+})
 function btn_1() {
     fetch('/content/vote/add1')
         .then(res => res.json())
         .then(vote1)
 }
-
 function vote1(vote){
     document.querySelector("#h_1").innerText = vote
     console.log(vote)
 }
 
 
+// ----------------vote2----------------------------
+document.addEventListener("DOMContentLoaded",function preview2() {
+    fetch('/content/vote/preview2')
+        .then(res => res.json())
+        .then(vote2)
+})
 function btn_2() {
-    h2 += 1
-    document.getElementById("h_2").innerText = h2
-    // if(h2 == 10){
-    //     alert('udah oiiiiii')
-    // }
+    fetch('/content/vote/add2')
+        .then(res => res.json())
+        .then(vote2)
 }
-function btn_3() {
-    h3 += 1
-    document.getElementById("h_3").innerText = h3
-    // if(h3 == 10){
-    //     alert('udah oiiiiii')
-    // }
+function vote2(vote){
+    document.querySelector("#h_2").innerText = vote
+    console.log(vote)
 }
 
-function nama_hari(){
-    let namahari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu","Minggu"]
-    let hari = new Date().getDay()
-    let x = namahari[hari-1]
-    console.log(`hari ini adalah ${x}`)
+
+// -----------------------vote3------------------
+document.addEventListener("DOMContentLoaded",function preview3() {
+    fetch('/content/vote/preview3')
+        .then(res => res.json())
+        .then(vote3)
+})
+function btn_3() {
+    fetch('/content/vote/add3')
+        .then(res => res.json())
+        .then(vote3)
 }
+function vote3(vote){
+    document.querySelector("#h_3").innerText = vote
+    console.log(vote)
+}
+
+
+
+
+
+
+
+
+
+// function nama_hari(){
+//     let namahari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu","Minggu"]
+//     let hari = new Date().getDay()
+//     let x = namahari[hari-1]
+//     console.log(`hari ini adalah ${x}`)
+// }
