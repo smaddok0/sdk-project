bc5502e1ef7ae762d4800a8938055bb371b48422
smaddok0 <tokekgoreng000@gmail.com>
2026-03-02 16:12:34 +0700

    tambah fitur daftar

--- web streaming/static/daftar/daftar.js
+++ web streaming/static/daftar/daftar.js
@@ -1,13 +1,39 @@
-const data_daftar = document.querySelector('#data_daftar')
-const input_daftar = document.querySelector('.input_daftar')
+const nama = document.querySelector('#nama')
+const pass = document.querySelector('#password')
+const submit = document.querySelector('#submit')
 
-
-data_daftar.addEventListener('keydown',function(i){
+nama.addEventListener("keydown", function(i){
     if(i.key === 'Enter'){
         i.preventDefault()
-        console.log('tidak terjadi apa apa')}
+        console.log('not submitted')}
 })
 
-data_daftar.addEventListener('input',function(i){
-    console.log('data terkirim')
+pass.addEventListener("keydown", function(i){
+    if(i.key === 'Enter'){
+        i.preventDefault()
+        console.log('not submitted')}
 })
+
+submit.addEventListener("click",() => {
+    fetch("/user/daftar/submit",{
+        method: 'POST',
+        headers:{'Content-type':'application/json'},
+        body: JSON.stringify({user:nama.value, password:pass.value})    
+    })
+    .then(res => res.json())
+    .then(hasil)
+})
+function hasil(hasil){
+    if(hasil['ada']){
+        alert(hasil.ada)
+    }
+    else{
+        if(hasil['pass']){
+            alert(hasil.pass)
+        }
+        else{
+            alert('berhasil daftar!!!, silahkan login')
+            window.location.href = hasil['redirect']
+        }
+    }
+}
