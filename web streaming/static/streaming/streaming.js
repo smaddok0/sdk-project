680ae731b72d3c9dbf7324724180f38029793748
smaddok0 <tokekgoreng000@gmail.com>
2026-03-03 01:27:20 +0700

    navbar and login/daftar update

--- web streaming/static/streaming/streaming.js
+++ web streaming/static/streaming/streaming.js
@@ -1,32 +1,37 @@
+const komen = document.querySelector('#type')
+const kirim = document.querySelector('#kirim')
+const base = document.querySelector('#cmd_b')
 
-let h1 = 0
-let h2 = 0
-let h3 = 0
-function btn_1() {
-    h1 += 1
-    document.getElementById("h1").innerText = h1
-    // if(h1 == 10){
-    //     alert('udah oiiiiii')
-    // }
-}
-function btn_2() {
-    h2 += 1
-    document.getElementById("h2").innerText = h2
-    // if(h2 == 10){
-    //     alert('udah oiiiiii')
-    // }
-}
-function btn_3() {
-    h3 += 1
-    document.getElementById("h3").innerText = h3
-    // if(h3 == 10){
-    //     alert('udah oiiiiii')
-    // }
+function command(user,text){
+    const box = document.createElement('div')
+    box.classList.add('box_cmd')
+
+    const user_cmd = document.createElement('span')
+    user_cmd.classList.add('user_cmd')
+    user_cmd.innerText = user
+
+    const cmd = document.createElement('p')
+    cmd.classList.add('cmd')
+    cmd.innerText = text
+
+    box.appendChild(user_cmd)
+    box.appendChild(cmd)
+    return box
 }
 
-function nama_hari(){
-    let namahari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu","Minggu"]
-    let hari = new Date().getDay()
-    let x = namahari[hari-1]
-    console.log(`hari ini adalah ${x}`)
+kirim.addEventListener('click', () =>{
+    fetch('/streaming/komen',{
+        method:'POST',
+        headers:{'Content-type':'application/json'},
+        body: JSON.stringify({komen : komen.value})
+    })
+    .then(res => res.json())
+    .then(hasil)    
+    komen.value = ""
+})
+
+function hasil(hasil){
+    base.appendChild(command('smaddok',hasil))
 }
+
+
